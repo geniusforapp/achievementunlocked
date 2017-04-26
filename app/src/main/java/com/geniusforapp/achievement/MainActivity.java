55c136f7c280f79036611bf7edd31683818fd195
ahmadnajar <geniusforapp@gmail.com>
2017-04-26 14:15:33 +0300

    Ädd check draw permission

--- app/src/main/java/com/geniusforapp/achievement/MainActivity.java
+++ app/src/main/java/com/geniusforapp/achievement/MainActivity.java
@@ -1,6 +1,11 @@
 package com.geniusforapp.achievement;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +19,8 @@ import com.geniusforapp.achievementunlocked.viewes.AchievementIconView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 1500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,17 +32,24 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AchievementUnlocked test = new AchievementUnlocked(MainActivity.this).setReadingDelay(5000).setDismissible(true);
-                AchievementData achievementData = new AchievementData();
-                achievementData.setTitle("Replace with your own action");
-                achievementData.setState(AchievementIconView.AchievementIconViewStates.FADE_DRAWABLE);
-                achievementData.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-                achievementData.setIcon(getResources().getDrawable(android.R.drawable.ic_dialog_alert));
-                achievementData.setTextColor(getResources().getColor(android.R.color.white));
-                test.show(achievementData);
+                if (checkDrawOverlayPermission()) {
+                    showAchievement();
+                }
+
 
             }
         });
+    }
+
+    private void showAchievement() {
+        AchievementUnlocked test = new AchievementUnlocked(MainActivity.this).setReadingDelay(5000).setDismissible(true);
+        AchievementData achievementData = new AchievementData();
+        achievementData.setTitle("Replace with your own action");
+        achievementData.setState(AchievementIconView.AchievementIconViewStates.FADE_DRAWABLE);
+        achievementData.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        achievementData.setIcon(getResources().getDrawable(android.R.drawable.ic_dialog_alert));
+        achievementData.setTextColor(getResources().getColor(android.R.color.white));
+        test.show(achievementData);
     }
 
     @Override
@@ -59,4 +73,30 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    public boolean checkDrawOverlayPermission() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        if (!Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, REQUEST_CODE);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    @TargetApi(Build.VERSION_CODES.M)
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            if (Settings.canDrawOverlays(this)) {
+                showAchievement();
+            }
+        }
+    }
+
+
 }
